9fca991be57cb91ef5cc8a552dc89de7c569fd16
sober_e1@qq.com <sober_e1@qq.com>
2018-03-29 21:51:35 +0800

    加了两页

---
+++ pages/depositdetail/depositdetail.js
@@ -0,0 +1,84 @@
+// pages/depositdetail/depositdetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    swiperInfo: {
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000,
+    },
+    depositTitle: [
+      { id: 1, url: 'test1.png' },
+      { id: 2, url: 'test1.png' },
+      { id: 3, url: 'test1.png' },
+      { id: 4, url: 'test1.png' },
+      { id: 5, url: 'test1.png' },
+      { id: 6, url: 'test1.png' }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
